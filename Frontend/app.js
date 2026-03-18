ea900958b8ae7d024b4843e0596747ae8db47eeb
Varad Jadhav <varadjadhav3663@gmail.com>
2026-03-18 12:09:03 +0530

    simple-application-for-docker-understanding

---
+++ Frontend/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+const URL = process.env.BACKEND_URL || 'http://localhost:4000/api';
+
+app.get('/', async function (req, res) {
+    try {
+        let response = await fetch(URL);   // ✅ direct fetch
+        response = await response.json();
+
+        console.log(response);
+
+        res.render('index', { data: response.data });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error");
+    }
+});
+
+app.listen(3000, function () {
+    console.log('Server running on http://localhost:3000');
+});
